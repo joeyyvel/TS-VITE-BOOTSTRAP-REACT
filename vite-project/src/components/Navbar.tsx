a487da227d4ac372cb1c9ab7a683338507d39188
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-11-22 09:34:37 +0800

    adding navbar

--- vite-project/src/components/Navbar.tsx
+++ vite-project/src/components/Navbar.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <div className="myNavbar">
+        <ul>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link className={item.cName} to={item.url}>
+                  <i className="{item.icon}"></i>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
 
--- vite-project/src/router.tsx
+++ vite-project/src/router.tsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Landing from "./routes/Home";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
+import SignUp from "./routes/SignUp";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/landing", element: <Landing /> },
+  { path: "/home", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/service", element: <Service /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "/signUp", element: <SignUp /> },
 ]);
